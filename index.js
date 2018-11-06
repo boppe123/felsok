ea8d5765621a1463277d152f3d7980f733a6e20b
boppe123 <44640763+boppe123@users.noreply.github.com>
2018-11-06 16:54:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -37,8 +37,13 @@ restService.post("/webhooktest", function(req, res) {
   });
   }*/
  }
- 
- 
+ if (Unit == 'lamp' && state == 'on' && cmd == 'turn'){
+	 callThingApiON().then((output) => {
+    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    res.json({ 'fulfillmentText': 'something is wrong' });
+  });
+ }
  else {
 	    returnError().then((ret) => {
     res.json({ 'fulfillmentText': ret }); // Return the results of the weather API to Dialogflow
